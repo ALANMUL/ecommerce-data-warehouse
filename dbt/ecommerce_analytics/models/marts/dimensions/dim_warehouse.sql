40ec25a3b17afb8757506118e931c506394fbc72
Al Anamul Mustakim <alanamulmustakim@gmail.com>
2026-09-14 10:53:47 +0600

    add region column to dim_warehouse

--- dbt/ecommerce_analytics/models/marts/dimensions/dim_warehouse.sql
+++ dbt/ecommerce_analytics/models/marts/dimensions/dim_warehouse.sql
@@ -1,1 +1,15 @@
-select * from {{ ref('stg_warehouses') }}
+select
+    warehouse_id,
+    warehouse_name,
+    city,
+    state,
+    case
+        when state in ('CA', 'OR', 'WA', 'NV', 'AZ', 'UT', 'CO', 'ID', 'MT', 'WY', 'NM') then 'West'
+        when state in ('NY', 'NJ', 'PA', 'MA', 'CT', 'RI', 'VT', 'NH', 'ME', 'MD', 'DE', 'VA', 'WV') then 'Northeast'
+        when state in ('TX', 'FL', 'GA', 'NC', 'SC', 'TN', 'AL', 'MS', 'LA', 'AR', 'KY', 'OK') then 'South'
+        when state in ('IL', 'OH', 'MI', 'IN', 'WI', 'MN', 'IA', 'MO', 'KS', 'NE', 'ND', 'SD') then 'Midwest'
+        else 'Other'
+    end as region,
+    warehouse_type,
+    capacity_units
+from {{ ref('stg_warehouses') }}
